782709b7a240c3afd293d346c4b7be15d1f71a3a
Void4413 <chandakaravali@gmail.com>
2023-05-23 09:00:44 +0530

    deleted comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import './App.css';
-//import {useMediaQuery} from 'react-responsive';
 import img2 from "./images/img2.png";
 import img1 from"./images/img1.png";
-// const Example = () => {
-//   const isDesktopOrLaptop = useMediaQuery({
-//   query: '(min-width: 1224px)'
-//   })
-//   const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' })
-//   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
-//   const isPortrait = useMediaQuery({ query: '(orientation: portrait)' })
-//   const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' })
-// }
 function App() {
   return (
     <div className="App" >
