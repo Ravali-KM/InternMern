4930b2a99e84ec193aae07aca2cff99091325100
Kotana Chandini <chandinikotana435@gmail.com>
2023-05-28 14:26:39 +0530

    initial comp2 commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponentOne from './ComponentOne/ComponentOne';
+function App() {
+  return (
+    <div className="App">
+      <ComponentOne/>
+    </div>
+  );
+}
+
+export default App;
