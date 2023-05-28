036daa6a7dcdaea14464c5f5d9b350e027573144
Void4413 <chandakaravali@gmail.com>
2023-05-28 16:39:45 +0530

    merge conflicts errors

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import Body from './Body/Body';
 import Footer from './Footer/Footer';
 import RR from './RR/rr';
 import React from 'react';
 import Bodycomp from './Bodycomp/comp3';
-=======
 import Componentfour from './Componentfour/Component4';
 
->>>>>>> origin/Siswina
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Bodycomp/>
+      <Componentfour/> 
       <Body/>
       <RR/>
       <Footer/>
-=======
-   <Componentfour/>  
->>>>>>> origin/Siswina
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> origin/Siswina
